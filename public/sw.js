b0a4f8774c53f5bbd2fe93e11ca6ae5e3c4f66a2
Harshil Tagline <harshil.tagline@gmail.com>
2022-12-15 12:40:02 +0530

    first commit

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// console.log("service worker from public");
+let cacheData = "pwaDemo";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll(["/static/js/bundle.js", "/index.html", "/users", "/", "/*"]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches.match(event.request).then((result) => {
+        if (result) {
+          return result;
+        }
+        let requestUrl = event.request.clone();
+        return fetch(requestUrl);
+      })
+    );
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Alert, Container, Nav, Navbar } from "react-bootstrap";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import "./App.css";
+import Home from "./components/Home";
+import Users from "./components/Users";
+import About from "./components/About";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("onLine");
+
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      setMode("onLine");
+    });
+    window.addEventListener("offline", () => {
+      setMode("offLine");
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/users">Users</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/about">About</Link>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        {mode === "offLine" && <Alert variant={"warning"}>Warning! You are offline.</Alert>}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users" element={<Users />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

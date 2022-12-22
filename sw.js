c32a1b4546996c301e000d79b85b29e2a5d438f4
Harshil Tagline <harshil.tagline@gmail.com>
2022-12-22 09:55:07 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-// console.log("service worker from public");
 let cacheData = "pwaDemo";
 
 this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
-      cache.addAll(["/static/js/bundle.js", "/index.html", "/users", "/"]);
-      // ["/static/js/bundle.js", "/index.html", "/users", "/", "/*"].map((el) => cache.add(el));
+      cache.addAll([
+        "/PWA_Demo/static/js/bundle.js",
+        "/PWA_Demo/static/js/main.chunk.js",
+        "/PWA_Demo/static/js/vendors~main.chunk.js",
+        "/PWA_Demo/index.html",
+        "/PWA_Demo/users",
+        "/PWA_Demo/about",
+        "/PWA_Demo/",
+        "/static/js/bundle.js",
+        "/static/js/main.chunk.js",
+        "/static/js/vendors~main.chunk.js",
+        "/index.html",
+        "/users",
+        "/about",
+        "/",
+      ]);
     })
   );
 });

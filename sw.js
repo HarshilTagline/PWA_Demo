c12599641c6a1bf16875a448991cde3914224bf5
Harshil Tagline <hsb@taglineinfotech.com>
2023-02-03 17:27:45 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
+// console.log("service worker from public");
 let cacheData = "pwaDemo";
 
 this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
-      cache.addAll([
-        "/PWA_Demo/static/js/bundle.js",
-        "/PWA_Demo/static/js/main.chunk.js",
-        "/PWA_Demo/static/js/vendors~main.chunk.js",
-        "/PWA_Demo/index.html",
-        "/PWA_Demo/users",
-        "/PWA_Demo/about",
-        "/PWA_Demo/",
-        "/static/js/bundle.js",
-        "/static/js/main.chunk.js",
-        "/static/js/vendors~main.chunk.js",
-        "/index.html",
-        "/users",
-        "/about",
-        "/",
-      ]);
+      cache.addAll(["/static/js/bundle.js", "/index.html", "/users", "/", "/about"]);
     })
   );
 });
